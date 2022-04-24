76cefe017650e53b8130f88cb9b08efdb1a88244
Soumik100 <104290141+Soumik100@users.noreply.github.com>
2022-04-24 10:27:22 +0530

    Add files via upload

---
+++ Assignment soln.py
@@ -0,0 +1,40 @@
+C:\python\python.exe C:/Users/shrey/PycharmProjects/Assignment.py/Assignment.py
+Enter first number: 56
+Enter second number: 78
+Enter third number: 152
+Entered values:  56 , 78 , 152
+Average of the given values:  95.33333333333333
+Enter your Gross income(in $): 200000
+Enter number of dependents in your family: 5
+Taxable income:  175000.0
+Tax: 35000.0
+Enter the total number of seconds: 86532
+Entered 86532 seconds = 1442 minutes 12 seconds
+Addition:  75
+<class 'str'>
+sin( 0 ): 0.0 , Cos( 0 ): 1.0
+sin( 15 ): 0.2588 , Cos( 15 ): 0.9659
+sin( 30 ): 0.5 , Cos( 30 ): 0.866
+sin( 45 ): 0.7071 , Cos( 45 ): 0.7071
+sin( 60 ): 0.866 , Cos( 60 ): 0.5
+sin( 75 ): 0.9659 , Cos( 75 ): 0.2588
+sin( 90 ): 1.0 , Cos( 90 ): 0.0
+sin( 105 ): 0.9659 , Cos( 105 ): -0.2588
+sin( 120 ): 0.866 , Cos( 120 ): -0.5
+sin( 135 ): 0.7071 , Cos( 135 ): -0.7071
+sin( 150 ): 0.5 , Cos( 150 ): -0.866
+sin( 165 ): 0.2588 , Cos( 165 ): -0.9659
+sin( 180 ): 0.0 , Cos( 180 ): -1.0
+sin( 195 ): -0.2588 , Cos( 195 ): -0.9659
+sin( 210 ): -0.5 , Cos( 210 ): -0.866
+sin( 225 ): -0.7071 , Cos( 225 ): -0.7071
+sin( 240 ): -0.866 , Cos( 240 ): -0.5
+sin( 255 ): -0.9659 , Cos( 255 ): -0.2588
+sin( 270 ): -1.0 , Cos( 270 ): -0.0
+sin( 285 ): -0.9659 , Cos( 285 ): 0.2588
+sin( 300 ): -0.866 , Cos( 300 ): 0.5
+sin( 315 ): -0.7071 , Cos( 315 ): 0.7071
+sin( 330 ): -0.5 , Cos( 330 ): 0.866
+sin( 345 ): -0.2588 , Cos( 345 ): 0.9659
+
+Process finished with exit code 0
