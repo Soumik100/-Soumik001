e1a4081ff36cf2e2162315ea4f1f37bb348c7217
Soumik100 <104290141+Soumik100@users.noreply.github.com>
2022-05-06 21:44:48 +0530

    Add files via upload

---
+++ Assignment 2 soln.py
@@ -0,0 +1,42 @@
+Windows PowerShell
+Copyright (C) Microsoft Corporation. All rights reserved.
+
+Try the new cross-platform PowerShell https://aka.ms/pscore6
+
+PS C:\Users\USER> & C:/Users/USER/AppData/Local/Programs/Python/Python310/python.exe "c:/Users/USER/OneDrive/Documents/Python ASSIGNMENT2. py.py"
+A) Length of the given string:  35 
+
+B) Reverse of the given string is :
+egaugnal evitisnes esac a si nohtyP 
+
+C) New sliced string is:  
+ a case sensitive 
+
+D) String after replacing:  
+ Python is object oriented language 
+
+E) Index of substring 'a' is: 10 
+
+F) Sting without white spaces:  
+ Pythonisacasesensitivelanguage
+Hey, Soumik Das Here!
+My SID is 21107113 
+I am from Mechanical Engineering department and my CGPA is 9.9
+A. a & b = 8
+B. a | b = 58
+C. a ^ b = 50
+D. a << 2 =  224
+E. a >> 2 =  14  and  b >> 4 =  0
+Enter any string: & C:/Users/USER/AppData/Local/Programs/Python/Python310/python.exe "c:/Users/USER/OneDrive/Documents/Python ASSIGNMENT2. py.py"
+No
+Enter First length : 81
+Enter Second length : 19
+Enter Third length : 90
+The triangle can be formed?
+Yes!
+Enter 1st number (a) 3     
+Enter 2nd number (b) 4
+Number of bits needed to be flipped to convert ‘a’ to ‘b’ are: 3
+
+
+
