22bf88077042cbeb58e9d55f0867dd519a9ec8bd
Soumik100 <104290141+Soumik100@users.noreply.github.com>
2022-05-12 21:34:23 +0530

    Add files via upload

---
+++ Answer assignment3.py
@@ -0,0 +1,53 @@
+Ans-1 :-
+Enter a number: 7
+Binary equivalent of 7 is 111
+Ans-2 :-
+Enter expression: (84+56-5*6)/9
+(84+56-5*6)/9 = 12.222222222222221
+Ans-3:-
+a) (3+4)(5) =  35
+Enter an integer = 3
+b) 3.0        
+Enter radius =50
+c) 31415.926535897932
+Enter angle 1 = 30   
+Enter angle 2 = 45
+d) 10.02574174024896
+Enter x coordinate of first point: 3
+Enter y coordinate of first point: 4
+Enter x coordinate of second point: 6
+Enter y coordinate of second point: 7
+e) 1.0  
+Ans-4 :-
+4.(a)   
+0       
+1       
+2       
+3       
+4       
+4.(b)   
+3       
+4       
+5       
+6       
+7       
+8       
+9       
+4.(c)   
+4       
+7       
+10      
+4.(d)
+15
+13
+11
+9
+7
+4.(e)
+Ans-5:-
+Enter number of carbon atoms in molecule: 1
+Enter number of hydrogen atoms in molecule: 6
+Enter number of oxygen atoms in molecule: 2
+The molecular weight of the compound is 50.057140000000004
+
+
