20003e3660fd082cd0a51f62a6c7ab22859b106f
Soumik100 <104290141+Soumik100@users.noreply.github.com>
2022-05-27 21:09:03 +0530

    Add files via upload

---
+++ soln assignment 4.py
@@ -0,0 +1,64 @@
+Q1
+Enter mark:56
+Grade: C      
+Continue?(y/n)n
+Q2
+Enter year:2002
+Not a leap year
+Q3
+Question 1:5x2=10
+Right!
+Question 2:6x4=24
+Right!
+Question 3:8x6=78
+Wrong. The correct answer is 48
+Question 4:0x3=0
+Right!
+Question 5:1x6=6
+Right!
+Question 6:7x9=54
+Wrong. The correct answer is 63
+Question 7:5x5=25
+Right!
+Question 8:9x5=67
+Wrong. The correct answer is 45
+Question 9:3x6=18
+Right!
+Question 10:2x3=4
+Wrong. The correct answer is 6
+Q4
+The number of pieces in the bowl are: 177
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
